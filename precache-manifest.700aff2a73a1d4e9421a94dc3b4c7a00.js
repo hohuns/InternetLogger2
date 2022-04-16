2683290a34ebfa63a018674ac2219bf7ba9486e8
hohuns <hohuns@hotmail.com>
2022-04-17 02:23:45 +0800

    Updates

---
+++ precache-manifest.700aff2a73a1d4e9421a94dc3b4c7a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e582467b1d0c7ba9d48ebc916dc507",
+    "url": "/InternetLogger2/index.html"
+  },
+  {
+    "revision": "875892cf9d3d5db63a9e",
+    "url": "/InternetLogger2/static/css/main.c633f605.chunk.css"
+  },
+  {
+    "revision": "94404f2a6258fe83f9a2",
+    "url": "/InternetLogger2/static/js/2.1c8c6549.chunk.js"
+  },
+  {
+    "revision": "b2b323168b47d3ad2cbed83af467569f",
+    "url": "/InternetLogger2/static/js/2.1c8c6549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875892cf9d3d5db63a9e",
+    "url": "/InternetLogger2/static/js/main.313253d8.chunk.js"
+  },
+  {
+    "revision": "9ba8a5582102b756ee6e",
+    "url": "/InternetLogger2/static/js/runtime-main.f4d42934.js"
+  }
+]);
